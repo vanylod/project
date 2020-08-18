bf8be76d545bb689679404eaae067dbddc2c1836
Ivan <example@gmail.com>
2020-08-18 20:06:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// var name = "Ivan";
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+
+// const  answer = prompt("Вам есть 18?");
+// console.log(answer);
+
+const answers = [];
+answers[0] = prompt('Как ваше имя?','');
+answers[1] = prompt('Как ваше фамилия?','');
+answers[2] = prompt('Сколько вам лет?','');
+
+document.write(answers);
